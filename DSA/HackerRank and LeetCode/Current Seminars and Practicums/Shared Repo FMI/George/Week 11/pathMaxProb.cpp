53dae1cbf294fc00d73c994204a822c02c811d6b
Alex Angelow <136594831+Deirror@users.noreply.github.com>
2024-12-20 17:16:24 +0200

    Update pathMaxProb.cpp

--- DSA/HackerRank and LeetCode/Current Seminars and Practicums/Shared Repo FMI/George/Week 11/pathMaxProb.cpp
+++ DSA/HackerRank and LeetCode/Current Seminars and Practicums/Shared Repo FMI/George/Week 11/pathMaxProb.cpp
@@ -1,19 +1,7 @@
-struct Edge {
-    int target;
-    double prob;
-};
-
-struct Node {
-    int index;
-    double prob;
-    bool operator<(const Node& rhs) const {
-        return prob < rhs.prob;
-    }
-};
 class Solution {
 public:
     double maxProbability(int n, vector<vector<int>>& edges, vector<double>& succProb, int start_node, int end_node) {
-        unordered_map<int, vector<Edge>> graph;
+        unordered_map<int, vector<pair<int, double>>> graph;
         for(size_t i = 0; i < edges.size(); i++) {
             graph[edges[i][0]].push_back({ edges[i][1], succProb[i] });
             graph[edges[i][1]].push_back({ edges[i][0], succProb[i] });
@@ -21,22 +9,22 @@ public:
         
         vector<double> probs(n, 0);
         probs[start_node] = 1;
-        priority_queue<Node> nextToProcess;
-        nextToProcess.push({start_node, 1});
+        priority_queue<pair<double, int>> nextToProcess;
+        nextToProcess.push({1., start_node});
         
         while(!nextToProcess.empty()) {
-            Node toProcess = nextToProcess.top();
+            auto toProcess = nextToProcess.top();
             nextToProcess.pop();
-            if(toProcess.prob > probs[toProcess.index]) continue;
-            if(toProcess.index == end_node) {
+            if(toProcess.first > probs[toProcess.second]) continue;
+            if(toProcess.second == end_node) {
                 return probs[end_node];
             }
 
-            for(auto& adj: graph[toProcess.index]) {
-                double possibleProb = probs[toProcess.index] * adj.prob;
-                if(probs[adj.target] < possibleProb) {
-                    probs[adj.target] = possibleProb;
-                    nextToProcess.push({ adj.target, possibleProb });
+            for(auto& adj: graph[toProcess.second]) {
+                double possibleProb = probs[toProcess.second] * adj.second;
+                if(probs[adj.first] < possibleProb) {
+                    probs[adj.first] = possibleProb;
+                    nextToProcess.push({ possibleProb, adj.first });
                 }
             }
         }
