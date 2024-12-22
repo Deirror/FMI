1adb53b2995aff9e206623878255bbebc60f5219
Alex Angelow <136594831+Deirror@users.noreply.github.com>
2024-12-22 14:16:45 +0200

    Update Prim_Kruskal.cpp

--- DSA/Algorithms/Minimal Spanning Tree/Prim_Kruskal.cpp
+++ DSA/Algorithms/Minimal Spanning Tree/Prim_Kruskal.cpp
@@ -41,7 +41,7 @@ unordered_map<int, vector<pair<int, int>>> prim(vector<vector<int>>& edges) {
 	priority_queue<pair<int, pair<int, int>>, vector<pair<int, pair<int, int>>>, greater<pair<int, pair<int, int>>>> pq;
 	add_edges(start_vertex, vertexes, graph, pq);
 	//assuming it is connected, otherwise inf cycle! 
-	while (vertexes.size() != graph.size()) {
+	while (vertexes.size() != graph.size() && pq.size()) {
 		auto minEdge = pq.top();
 		pq.pop();
 		int weight = minEdge.first;
@@ -54,6 +54,78 @@ unordered_map<int, vector<pair<int, int>>> prim(vector<vector<int>>& edges) {
 		mst[child].push_back({parent, weight});
 		vertexes.insert(child);
 		add_edges(child, vertexes, graph, pq);
+	}
+	return mst;
+}
+
+class DSU {
+private:
+	vector<int> parent, rank;
+
+public:
+	DSU(int n) {
+		parent.resize(n);
+		rank.resize(n, 0);
+		for (int i = 0; i < n; i++) {
+			parent[i] = i;
+		}
+	}
+
+	int find(int u) {
+		if (u != parent[u]) {
+			parent[u] = find(parent[u]);
+		}
+		return parent[u];
+	}
+
+	bool unionSets(int u, int v) {
+		int rootU = find(u);
+		int rootV = find(v);
+		if (rootU == rootV) {
+			return false;
+		}
+		if (rank[rootU] < rank[rootV]) {
+			parent[rootU] = rootV;
+		} else if (rank[rootU] > rank[rootV]) {
+			parent[rootV] = rootU;
+		} else {
+			parent[rootV] = rootU;
+			rank[rootU]++;
+		}
+		return true;
+	}
+};
+
+unordered_map<int, vector<pair<int, int>>> kruskal(vector<vector<int>>& edges) {
+	priority_queue<pair<int, pair<int, int>>, vector<pair<int, pair<int, int>>>, greater<pair<int, pair<int, int>>>> pq;
+	unordered_map<int, vector<pair<int, int>>> mst;
+
+	size_t n = 0;
+	{
+		unordered_set<int> vertexes;
+		for (auto& edge : edges) {
+			pq.push({ edge[2], {edge[0], edge[1]} });
+			vertexes.insert(edge[0]);
+			vertexes.insert(edge[1]);
+		}
+		n = vertexes.size();
+	}
+
+	DSU dsu(n);
+	int edgesCount = 0;
+
+	while (!pq.empty() && edgesCount < n - 1) {
+		auto minEdge = pq.top();
+		pq.pop();
+		int weight = minEdge.first;
+		int u = minEdge.second.first;
+		int v = minEdge.second.second;
+
+		if (dsu.unionSets(u, v)) {
+			mst[u].push_back({v, weight});
+			mst[v].push_back({u, weight});
+			edgesCount++;
+		}
 	}
 	return mst;
 }
@@ -72,14 +144,14 @@ unordered_map<int, vector<pair<int, int>>> find_msp(Algorithm algorithm) {
 		return prim(edges);
 	}
 	case Algorithm::Kruskal: {
-
+		return kruskal(edges);
 	}
 	}
 	return {};
 }
 
 int main() {
-	auto mst = find_msp(Algorithm::Prim);
+	auto mst = find_msp(Algorithm::Kruskal);
 	for (auto& kvp : mst) {
 		cout << kvp.first << ":\n";
 		for (auto& edge : kvp.second) {
