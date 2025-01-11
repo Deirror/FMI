078f94d9bad141239976b927471115c0b9bee6f0
Alex Angelow <136594831+Deirror@users.noreply.github.com>
2025-01-11 17:34:22 +0200

    Update buildPath.cpp

--- DSA/HackerRank and LeetCode/Current Seminars and Practicums/From Homeworks/HW 11/buildPath.cpp
+++ DSA/HackerRank and LeetCode/Current Seminars and Practicums/From Homeworks/HW 11/buildPath.cpp
@@ -82,3 +82,80 @@ int main() {
     }
     return 0;
 }
+
+
+/*
+    2.KRUSKAL ALGORITHM
+*/
+
+class DSU {
+    vector<int> parent, size;
+public:
+    DSU(int n) : parent(n + 1), size(n + 1, 1) {
+        for(int i = 0; i <= n; i++) {
+            parent[i] = i;
+        }
+    }
+    
+    int find(int u) {
+        if(u != parent[u]) {
+            return parent[u] = find(parent[u]);
+        }
+        return u;
+    }
+    
+    void unionSets(int u, int v) {
+        int ru = find(u);
+        int rv = find(v);
+        if(ru == rv) {
+            return;
+        }
+        if(size[ru] > size[rv]) {
+            parent[rv] = ru;
+            size[ru] += size[rv];
+        } else {
+            parent[ru] = rv;
+            size[rv] += size[ru];
+        }
+    }
+};
+
+vector<Path> kruskal(int n, int m, vector<Path>& paths) {
+    sort(paths.begin(), paths.end());
+    DSU dsu(n);
+    vector<Path> mst;
+    for(int i = 0; i < m; i++) {
+        //cout << paths[i].idx << ' ';
+        if(dsu.find(paths[i].from) == dsu.find(paths[i].to)) {
+            continue;
+        }
+        dsu.unionSets(paths[i].from, paths[i].to);
+        mst.emplace_back(paths[i]);
+        if(mst.size() == n - 1) {
+            break;
+        }
+    }
+    return mst;
+}
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
+    int n, m;
+    cin >> n >> m;
+    vector<Path> paths;
+    for(int i = 1; i <= m; i++) {
+        int x, y, c1, c2;
+        cin >> x >> y >> c1 >> c2;
+        paths.emplace_back(i, x, y, c1, c2);
+    }
+    vector<Path> mst = kruskal(n, m, paths);
+    priority_queue<int, vector<int>, greater<int>> pq;
+    for(auto& path : mst) {
+        pq.push(path.idx);
+    }
+    while(pq.size()) {
+        cout << pq.top() << '\n';
+        pq.pop();
+    }
+    return 0;
+}
