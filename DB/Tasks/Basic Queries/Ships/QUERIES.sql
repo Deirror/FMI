330760d776f106f0627b5c95c1c948b68b0da923
Alex Angelow <136594831+Deirror@users.noreply.github.com>
2025-02-18 21:34:58 +0200

    finish Ship qs

---
+++ DB/Tasks/Basic Queries/Ships/QUERIES.sql
@@ -0,0 +1,19 @@
+USE Ships
+
+--Напишете заявка, която извежда класа и страната за всички класове с помалко от 10 оръдия.
+SELECT class, country FROM Classes WHERE numguns < 10
+
+--Напишете заявка, която извежда имената на корабите, пуснати на вода преди 1918. Задайте псевдоним shipName на колоната.
+SELECT name AS shipName FROM Ships WHERE launched < 1918
+
+--Напишете заявка, която извежда имената на корабите потънали в битка и имената на съответните битки.
+SELECT ship, battle FROM Outcomes WHERE result='sunk'
+
+--Напишете заявка, която извежда имената на корабите с име, съвпадащо с името на техния клас.
+SELECT name FROM Ships WHERE class=name
+
+--Напишете заявка, която извежда имената на корабите, които започват с буквата R.
+SELECT name FROM Ships WHERE name LIKE 'R%'
+
+--Напишете заявка, която извежда имената на корабите, които съдържат 2 или повече думи.
+SELECT name FROM Ships WHERE name LIKE '% %'
