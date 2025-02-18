f4d559b748abab27f450af9bf76448b19ba7718f
Alex Angelow <136594831+Deirror@users.noreply.github.com>
2025-02-18 20:49:56 +0200

    add task queries

---
+++ DB/Tasks/Basic Queries/Movies/Studio/QUERIES.sql
@@ -0,0 +1,8 @@
+USE Movies
+
+--Напишете заявка, която извежда адреса на студио ‘Disney’
+SELECT address FROM Studio WHERE name='Disney'
+
+--Напишете заявка, която извежда рождената дата на актьора Jack Nicholson
+SELECT birthdate FROM MovieStar WHERE name='Jack Nicholson'
+
