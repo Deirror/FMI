f11ef205c4332383d4e73ba7ff37ce9ce744054d
Alex Angelow <136594831+Deirror@users.noreply.github.com>
2025-02-18 12:09:31 +0200

    Update 02-a-4000.sh

--- OS/Shell/Exercises/Commands/02-a-4000.sh
+++ OS/Shell/Exercises/Commands/02-a-4000.sh
@@ -1,1 +1,5 @@
 #Създайте файл permissions.txt в home директорията си. За него дайте единствено - read права на потребителя създал файла, write and exec на групата, read and exec на всички останали. Направете го и с бит
+
+touch ~/permissions.txt
+chmod u=r,g=wx,o=rx ~/permissions.txt
+chmod 435 ~/permissions.txt # same as above but in octal
