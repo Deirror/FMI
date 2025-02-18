7a286a64289b453a55a2e6c92152ca723c979dbc
Alex Angelow <136594831+Deirror@users.noreply.github.com>
2025-02-18 17:33:59 +0200

    Update CREATE_TABLE.sql

--- DB/Tasks/Basic Queries/Movies/CREATE_TABLE.sql
+++ DB/Tasks/Basic Queries/Movies/CREATE_TABLE.sql
@@ -1,5 +1,10 @@
 USE WEEK_01
 
-CREATE TABLE Movies (
-  
+CREATE TABLE Movie (
+	title TEXT,
+	year INT,
+	length INT,
+	incolor BIT,
+	studioname FOREIGN KEY REFERENCES Studio(name),
+	producerc# FOREIGN KEY REFERENCES MovieExec(cert#)
 )
