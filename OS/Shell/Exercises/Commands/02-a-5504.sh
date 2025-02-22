89c4159eac6e1343fe98b205739733441e192dae
Alex Angelow <136594831+Deirror@users.noreply.github.com>
2025-02-22 21:48:39 +0200

    Update 02-a-5504.sh

--- OS/Shell/Exercises/Commands/02-a-5504.sh
+++ OS/Shell/Exercises/Commands/02-a-5504.sh
@@ -1,3 +1,7 @@
 # Изведете всички обикновени файлове в директорията /tmp които са от вашата група, които имат write права за достъп за група или за останалите(o=w)
 
 find /tmp -type f -group $(id -g) \( -perm g=w -o -perm g=w \) 
+
+# or
+
+find /tmp/ -type f -group $(id -g) -perm /022 2>/dev/null
