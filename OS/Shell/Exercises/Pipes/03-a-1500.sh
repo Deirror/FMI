3a70cfa29e378336c1fb904894e837dd3d337e79
Alex Angelow <136594831+Deirror@users.noreply.github.com>
2025-02-24 13:12:50 +0200

    Create 03-a-1500.sh

---
+++ OS/Shell/Exercises/Pipes/03-a-1500.sh
@@ -0,0 +1,12 @@
+# Изведете броя на байтовете в /etc/passwd.
+
+wc -c /etc/passwd | cut -d ' ' -f 1
+
+
+# Изведете броя на символите в /etc/passwd.
+
+wc -m /etc/passwd | cut -d ' ' -f 1
+
+# Изведете броя на редовете  в /etc/passwd.
+
+wc -l /etc/passwd | cut -d ' ' -f 1
