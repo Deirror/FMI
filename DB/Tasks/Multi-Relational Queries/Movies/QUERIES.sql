63b92c2af71f5824037b372851516f08df627394
Alex Angelow <136594831+Deirror@users.noreply.github.com>
2025-02-25 19:13:46 +0200

    Update QUERIES.sql

--- DB/Tasks/Multi-Relational Queries/Movies/QUERIES.sql
+++ DB/Tasks/Multi-Relational Queries/Movies/QUERIES.sql
@@ -1,9 +1,9 @@
 USE Movies
 
---Напишете заявка, която извежда имената на актьорите мъже, участвали във филма The Usual Suspects.
+-- Напишете заявка, която извежда имената на актьорите мъже, участвали във филма The Usual Suspects
 SELECT MovieStar.name FROM MovieStar 
 JOIN StarsIn
 ON StarsIn.starname=MovieStar.name
 WHERE MovieStar.gender='M' AND StarsIn.movietitle='The Usual Suspects'
 
--- 
+-- Напишете заявка, която извежда имената на актьорите, участвали във филми от 1995, продуцирани от студио MGM
