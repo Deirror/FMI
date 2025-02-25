0fb541528557defa74be10671eaf093bf7f6d34d
Alex Angelow <136594831+Deirror@users.noreply.github.com>
2025-02-25 19:05:30 +0200

    Update QUERIES.sql

--- DB/Tasks/Multi-Relational Queries/Movies/QUERIES.sql
+++ DB/Tasks/Multi-Relational Queries/Movies/QUERIES.sql
@@ -1,1 +1,9 @@
+USE Movies
 
+-- Напишете заявка, която извежда имената на актьорите мъже, участвали във филма The Usual Suspects.
+SELECT MovieStar.name FROM MovieStar 
+JOIN StarsIn
+ON StarsIn.starname=MovieStar.name
+WHERE MovieStar.gender='M' AND StarsIn.movietitle='The Usual Suspects'
+
+-- 
