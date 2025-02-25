eb1a195b2370a00d19677937450cb315f576f512
Alex Angelow <136594831+Deirror@users.noreply.github.com>
2025-02-25 12:30:12 +0200

    Update 03-b-6100.sh

--- OS/Shell/Exercises/Pipes/03-b-6100.sh
+++ OS/Shell/Exercises/Pipes/03-b-6100.sh
@@ -1,2 +1,7 @@
 # Отпечатайте последната цифра на UID на всички редове между 28-ми и 46-ред в /etc/passwd
 
+sed -n '28,46p' /etc/passwd | cut -d ':' -f 3 | awk '{print substr($0, length($0), 1)}'
+
+# или
+
+ cut -d ':' -f 3 /etc/passwd | head -n 46 | tail -n 28 | sed 's/.*\(.\)$/\1/'
