a11cf466e2d41df0a58cb903d4370bbaeba4822c
Alex Angelow <136594831+Deirror@users.noreply.github.com>
2025-02-26 21:11:30 +0200

    Update 03-b-9500.sh

--- OS/Shell/Exercises/Pipes/03-b-9500.sh
+++ OS/Shell/Exercises/Pipes/03-b-9500.sh
@@ -1,42 +1,45 @@
-# Копирайте <РЕПО>/exercises/data/ssa-input.txt във вашата home директория.
+# Копирайте <РЕПО>/exercises/data/ssa-input.txt във вашата home директория
+cp /srv/fmi-os/exercises/data/ssa-input.txt ~/ssa
+
 # Общият вид на файла е:
 
+# - заглавна част:
+#	Smart Array P440ar in Slot 0 (Embedded)
 
-- заглавна част:
-	Smart Array P440ar in Slot 0 (Embedded)
+# - една или повече секции за масиви:
+#	Array A
+#	Array B
+#	...
+#	като буквата (A, B, ...) е името на масива
 
-- една или повече секции за масиви:
-	Array A
-	Array B
-	...
-	като буквата (A, B, ...) е името на масива
+# - във всяка таква секция има една или повече подсекции за дискове:
+#	physicaldrive 2I:0:5
+#	physicaldrive 2I:0:6
+#	...
 
-- във всяка таква секция има една или повече подсекции за дискове:
-	physicaldrive 2I:0:5
-	physicaldrive 2I:0:6
-	...
+#	като 2I:0:5 е името на диска
 
-	като 2I:0:5 е името на диска
+# - във всяка подсекция за диск има множество параметри във вида:
+#	key name: value
+#	като за нас са интересни само:
 
-- във всяка подсекция за диск има множество параметри във вида:
-	key name: value
-	като за нас са интересни само:
+#		Current Temperature (C): 35
+#		Maximum Temperature (C): 36
 
-		Current Temperature (C): 35
-		Maximum Temperature (C): 36
+# Напишете поредица от команди която обработва файл в този формат, и генерира
+# следният изход:
 
-Напишете поредица от команди която обработва файл в този формат, и генерира
-следният изход:
+# A-2I:0:5 35 36
+# A-2I:0:6 34 35
+# B-1I:1:1 35 50
+# B-1I:1:2 35 49
 
-A-2I:0:5 35 36
-A-2I:0:6 34 35
-B-1I:1:1 35 50
-B-1I:1:2 35 49
+# x-yyyyyy zz ww
 
-x-yyyyyy zz ww
+# където:
+#	- x е името на масива
+#	- yyyyyy е името на диска
+#	- zz е current temperature
+#	- ww е max temperature
 
-където:
-	- x е името на масива
-	- yyyyyy е името на диска
-	- zz е current temperature
-	- ww е max temperature
+grep -E "(Array|physicaldrive|Temperature)" ssa | sed 's/^[ ]*//g' | awk -F ' ' 'BEGIN{arr} {if($1=="Array") {arr=$2} else if($1=="physicaldrive") {print arr"-"$2;} else if($2=="Temperature") {print $4;} }'
