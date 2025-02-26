1de213a8e557cff0d886b72a005153488872b9b4
Alex Angelow <136594831+Deirror@users.noreply.github.com>
2025-02-26 09:21:56 +0200

    Update 03-b-9052.sh

--- OS/Shell/Exercises/Pipes/03-b-9052.sh
+++ OS/Shell/Exercises/Pipes/03-b-9052.sh
@@ -1,3 +1,3 @@
 # Използвайки файл population.csv, намерете през коя година в България има най-много население
 
-grep "Bulgaria" ~/ppl | sort -t ',' -k 4 -nr | head -n 1
+grep "Bulgaria" ~/ppl | sort -t ',' -k 4 -nr | head -n 1 | cut -d ',' -f 3
