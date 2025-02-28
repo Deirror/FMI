fff001f950f98946d6dfe5852e5905df6504ab43
Alex Angelow <136594831+Deirror@users.noreply.github.com>
2025-02-28 23:15:29 +0200

    Update 2023-SE-02.sh

--- OS/Shell/Collections/2023-SE-02.sh
+++ OS/Shell/Collections/2023-SE-02.sh
@@ -1,3 +1,4 @@
 # Напишете серия от команди, намираща общия брой на срещанията на думата error във всички log файлове
 
-find /var/log/my_log -type f -regex "./[a-zA-Z0-9_]+_[0-9]+\.log"
+find /var/log/my_log -type f -regex "./[a-zA-Z0-9_]+_[0-9]+\.log" | xargs -r -I{} cat {} | tr ' ' '\n' \
+| sort | uniq -ic | grep "error" | cut -d ' ' -f 1
