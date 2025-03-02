883ff01d79fa20bf2e7b9a07ec9c67fed89ddfdd
Alex Angelow <136594831+Deirror@users.noreply.github.com>
2025-03-02 19:13:45 +0200

    Update 05-b-3400.sh

--- OS/Shell/Exercises/Scripts/05-b-3400.sh
+++ OS/Shell/Exercises/Scripts/05-b-3400.sh
@@ -3,3 +3,23 @@
 
 # NB! Символният низ може да съдържа интервал (' ') в себе си.
 
+#!/bin/bash
+
+if [[ ${#} -lt 2 ]]; then
+    echo "Expected 2 arguments"
+    exit 1
+fi
+
+filename=${1}
+shift
+
+if [[ ! -f ${filename} ]]; then
+    echo "File doesn't exist"
+    exit 2
+fi
+
+if grep -q "${*}" ${filename}; then
+    echo "${filename} contains ${*}"
+else
+    echo "${filename} doesn't contain ${*}"
+fi
