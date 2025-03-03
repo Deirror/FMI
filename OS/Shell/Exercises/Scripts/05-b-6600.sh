2efa76dc9b2168a7df96ed5ec0b93120a160b89c
Alex Angelow <136594831+Deirror@users.noreply.github.com>
2025-03-03 18:19:29 +0200

    Create 05-b-6600.sh

---
+++ OS/Shell/Exercises/Scripts/05-b-6600.sh
@@ -0,0 +1,32 @@
+# Да се напише shell скрипт, който получава единствен аргумент директория и изтрива всички повтарящи се
+# (по съдържание) файлове в дадената директория. Когато има няколко еднакви файла, да се остави само този,
+# чието име е лексикографски преди имената на останалите дублирани файлове.
+
+#!/bin/bash
+
+if [[ $# -ne 1 ]] ; then
+        echo "Expected 1 argument - directory"
+        exit 1
+fi
+
+if [[ ! -d ${1} ]] ; then
+        echo "Expected 1 argument - directory"
+        exit 1
+fi
+
+for file in $(find "${1}" -mindepth 1 -maxdepth 1 -type f) ; do
+        if [[ -f "${file}" ]] ; then
+                for FILE in $(find "${1}" -mindepth 1 -maxdepth 1 -type f) ; do
+                        if [[ -f "${FILE}" ]] && [[ -f "${file}" ]] && [[ "${file}" != "${FILE}" ]] ; then
+                                diff -q "${file}" "${FILE}" >/dev/null
+                                if [[ ${?} -eq 0 ]] ; then
+                                        if [[ "${file}" > "${FILE}" ]] ; then
+                                                rm "${file}"
+                                        else
+                                                rm "${FILE}"
+                                        fi
+                                fi
+                        fi
+                done
+        fi
+done
