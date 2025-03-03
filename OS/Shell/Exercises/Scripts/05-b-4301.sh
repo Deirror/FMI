026887feb6e8240e2d2127d8f761911c6bc1cf36
Alex Angelow <136594831+Deirror@users.noreply.github.com>
2025-03-03 10:52:36 +0200

    Update 05-b-4301.sh

--- OS/Shell/Exercises/Scripts/05-b-4301.sh
+++ OS/Shell/Exercises/Scripts/05-b-4301.sh
@@ -14,3 +14,45 @@
 # всички те да се показват на потребителя, заедно с пореден номер >=1,
 # след което той да може да въведе някой от номерата (или 0 ако не си хареса никого), и само избраният да бъде добавен
 # към указателя.
+
+#!/bin/bash
+
+if [[ $# -ne 3 ]] ; then
+        echo "Expected 3 arguments"
+        exit 1
+fi
+
+fileName=${1}
+fullName=${2}
+nickName=${3}
+
+userName=$(grep "${fullName}" /etc/passwd | awk -F ':' '{print $1}')
+candidates=$(echo "${userName}" | wc -l)
+
+if [[ ${userName} == "" ]] ; then
+        echo "User not found"
+        exit 1
+fi
+
+if [[ ${candidates} -gt 1 ]] ; then
+        index=1
+        while read line ; do
+                echo "${index}.${line}"
+                arr[index]=${line}
+                index=$(( index + 1 ))
+        done < <(echo "${userName}")
+        echo "0.Exit"
+        read -p "Choose from 1-${candidates}: " choice
+        if [[ ${choice} -lt 0 ]] || [[ ${choice} -gt ${candidates} ]] ; then
+                echo "Invalid choice"
+                exit 1
+        else
+                if [[ ${choice} -eq 0 ]] ; then
+                        exit 0
+                else
+                        userName=${arr[${choice}]}
+                fi
+        fi
+fi
+
+echo "${nickName} ${userName}" >> ${fileName}
