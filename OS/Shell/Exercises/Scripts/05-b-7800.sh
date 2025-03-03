75f355e1916517b2f857e62e5ccc89462d56b309
Alex Angelow <136594831+Deirror@users.noreply.github.com>
2025-03-03 22:20:45 +0200

    Create 05-b-7800.sh

---
+++ OS/Shell/Exercises/Scripts/05-b-7800.sh
@@ -0,0 +1,24 @@
+# Да се напише shell скрипт, който намира броя на изпълнимите файлове в PATH.
+# Hint: Предполага се, че няма спейсове в имената на директориите
+# Hint2: Ако все пак искаме да се справим с този случай, да се разгледа IFS променливата и констуркцията while read -d
+
+#!/bin/bash
+
+if [[ $# -ne 1 ]] || [[ ! -d ${1} ]]; then
+        echo "Expected 1 argument - directory"
+        exit 1
+fi
+
+counter=0
+
+for file in $(find ${1} -mindepth 1 -maxdepth 1 -type f) ; do
+        if [[ -x ${file} ]] ; then
+                counter=$(( counter + 1 ))
+        fi
+done
+
+echo "${counter}"
+
+# или
+
+find $1 -mindepth 1 -maxdepth 1 -type f -executable | wc -l
