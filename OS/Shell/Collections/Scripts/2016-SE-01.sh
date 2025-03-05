5ff1db2e38d082f772569d005cda6810de882fde
Alex Angelow <136594831+Deirror@users.noreply.github.com>
2025-03-05 10:24:19 +0200

    Update 2016-SE-01.sh

--- OS/Shell/Collections/Scripts/2016-SE-01.sh
+++ OS/Shell/Collections/Scripts/2016-SE-01.sh
@@ -1,3 +1,20 @@
 # Напишете shell скрипт, който по подаден един позиционен параметър, ако този параметър
 # е директория, намира всички symlink-ове в нея и под-директориите ѝ с несъществуващ destination
 
+#!/bin/bash
+
+if [[ $# -ne 1 ]]; then
+    echo "Only one arg needed"
+    exit 1
+fi
+
+if [[ ! -d "$1" ]]; then
+    echo "Not a directory"
+    exit 2
+fi
+
+while read file; do
+    if [[ ! -e "$file" ]]; then
+        echo "$file"
+    fi
+done < <(find "$1" -type l 2>/dev/null | xargs -I{} readlink {})
