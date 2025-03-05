1a6698dde4faeb27d4e221f1496327421c785597
Alex Angelow <136594831+Deirror@users.noreply.github.com>
2025-03-05 19:14:58 +0200

    Update QUERIES.sql

--- DB/Tasks/Subqueries/Ships/QUERIES.sql
+++ DB/Tasks/Subqueries/Ships/QUERIES.sql
@@ -5,3 +5,22 @@ SELECT DISTINCT Classes.country FROM Classes
 WHERE Classes.numguns=(
 	SELECT MAX(Classes.numguns) FROM Classes
 )
+
+-- Напишете заявка, която извежда класовете, за които поне един от корабите е потънал в битка
+SELECT DISTINCT Classes.class FROM Classes
+JOIN Ships
+ON Ships.class=Classes.class
+JOIN Outcomes
+ON Outcomes.ship=Ships.name
+WHERE Ships.name IN (
+	SELECT Outcomes.ship FROM Outcomes
+	WHERE Outcomes.result='sunk'
+)
+
+-- Напишете заявка, която извежда името и класа на корабите с 16 инчови оръдия
+SELECT Ships.name, Classes.class FROM Ships
+JOIN Classes
+ON Classes.class=Ships.class
+WHERE Classes.bore=16
+
+-- Напишете заявка, която извежда имената на битките, в които са участвали кораби от клас ‘Kongo’
