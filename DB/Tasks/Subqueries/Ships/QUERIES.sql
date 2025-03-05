f90d4a25efef89c8acc15ae2ccc9d57a4ae4b9f1
Alex Angelow <136594831+Deirror@users.noreply.github.com>
2025-03-05 18:53:58 +0200

    Update QUERIES.sql

--- DB/Tasks/Subqueries/Ships/QUERIES.sql
+++ DB/Tasks/Subqueries/Ships/QUERIES.sql
@@ -1,1 +1,7 @@
+USE Ships
 
+-- Напишете заявка, която извежда страните, чиито кораби са с най-голям брой оръдия
+SELECT DISTINCT Classes.country FROM Classes
+WHERE Classes.numguns=(
+	SELECT MAX(Classes.numguns) FROM Classes
+)
