02e2b1906973db1c24991ad4f69f5381e6ac3254
Alex Angelow <136594831+Deirror@users.noreply.github.com>
2025-03-05 18:31:17 +0200

    Update QUERIES.sql

--- DB/Tasks/Subqueries/PC/QUERIES.sql
+++ DB/Tasks/Subqueries/PC/QUERIES.sql
@@ -19,3 +19,27 @@ WHERE Laptop.speed < ALL(
 )
 
 -- Напишете заявка, която извежда модела и цената на продукта (PC, лаптоп или принтер) с най-висока цена
+SELECT TOP 1 Product.model, MAX(COALESCE(PC.price, Laptop.price, Printer.price)) AS price FROM Product 
+LEFT JOIN PC
+ON PC.model=Product.model
+LEFT JOIN Laptop
+ON Laptop.model=Product.model
+LEFT JOIN Printer
+ON Printer.model=Product.model
+GROUP BY Product.model
+ORDER BY price DESC
+
+-- Напишете заявка, която извежда производителя на цветния принтер с най-ниска цена
+SELECT TOP 1 Product.maker FROM Printer
+JOIN Product
+ON Product.model=Printer.model
+WHERE Printer.color='y'
+ORDER BY Printer.price
+
+-- Напишете заявка, която извежда производителите на тези персонални 
+-- компютри с най-малко RAM памет, които имат най-бързи процесори
+SELECT DISTINCT Product.maker
+FROM Product
+JOIN PC ON Product.model=PC.model
+WHERE PC.ram=(SELECT MIN(PC.ram) FROM PC)
+AND PC.speed=(SELECT MAX(PC.speed) FROM PC WHERE PC.ram = (SELECT MIN(PC.ram) FROM PC))
