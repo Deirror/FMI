136477fa6307f1ef62cf5cadf4c91e1e3e88ab83
Alex Angelow <136594831+Deirror@users.noreply.github.com>
2025-03-05 15:53:13 +0200

    Update QUERIES.sql

--- DB/Tasks/Subqueries/PC/QUERIES.sql
+++ DB/Tasks/Subqueries/PC/QUERIES.sql
@@ -1,1 +1,21 @@
+USE PC
 
+-- Напишете заявка, която извежда производителите на персонални компютри с честота над 500
+SELECT DISTINCT Product.maker FROM Product
+JOIN PC
+ON PC.model=Product.model
+WHERE PC.speed > 500
+
+-- Напишете заявка, която извежда код, модел и цена на принтерите с найвисока цена
+SELECT Printer.code, Printer.model, Printer.price FROM Printer
+WHERE Printer.price=(
+	SELECT MAX(Printer.price) FROM Printer
+)
+
+-- Напишете заявка, която извежда лаптопите, чиято честота е по-ниска от честотата на всички персонални компютри
+SELECT * FROM Laptop
+WHERE Laptop.speed < ALL(
+    SELECT PC.speed FROM PC
+)
+
+-- Напишете заявка, която извежда модела и цената на продукта (PC, лаптоп или принтер) с най-висока цена
