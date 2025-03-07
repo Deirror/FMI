f2e2108fe3ac58fe80e5c2950fcab560fdef3f2d
Alex Angelow <136594831+Deirror@users.noreply.github.com>
2025-03-07 09:27:00 +0200

    Update 2024-SE-06.sh

--- OS/Shell/Collections/Scripts/2024-SE-06.sh
+++ OS/Shell/Collections/Scripts/2024-SE-06.sh
@@ -4,26 +4,52 @@
 #!/bin/bash
 
 if [[ $# -ne 1 ]]; then
-    echo "Exactly one argument required"
+    echo "ERR> Exactly one argument required"
     exit 1
 fi
 
 SYS_STATUS_FILE="$1"
 
 if [[ ! -f $SYS_STATUS_FILE ]]; then
-    echo "File with wanted file sys status doesn't exist"
+    echo "ERR> File - wanted file sys status - doesn't exist"
     exit 2
 fi
 
 while read LINE; do
     ARGS_COUNT=$(sed "s/ /\n/g" | wc -l)
     if [[ $ARGS_COUNT -lt 2 || $ARGS_COUNT -gt 4]]; then
-        echo "Invalid number of arguments - $LINE"
-        CONTINUE
+        echo "ERR> Invalid number of arguments - $LINE"
+        contimue
     fi
     FILE_PATH=$(cut -d ' ' -f 1)
     TYPE=$(cut -d ' ' -f 2)
-    case "$TYPE" in
+    if [[ $# -eq 2 ]]; then
+        if [[ "$TYPE" != "nonexistant" ]]; then
+            echo "ERR> Invalid type on line - $LINE"
+            continue
+        fi
+        if [[ -e "$FILE_PATH" ]]; then
+            echo "ERR> File exists on line - $LINE"
+            rm -ri "$FILE_PATH"
+        else
+            echo "OK> File is nonexistant"
+        fi
+    else
+        if [[ $# -eq 3 ]]; then
+            PERM=$(cut -d ' ' -f 3)
+            if [[ ! -e "$FILE_PATH" ]]; then
+                echo "> Creating file $FILE_PATH"
+            fi
+        else
+            OWNERS=$(cut -d ' ' -f 3) 
+            PERM=$(cut -d ' ' -f 4) 
+        fi
+    fi
+done < <(cat "$SYS_STATUS_FILE")
+
+
+# ------------------
+ case "$TYPE" in
     "file")
     ;;
     "dir")
@@ -35,4 +61,3 @@ while read LINE; do
     *)
     echo "Unknown type - $LINE";;
     esac
-done < <(cat "$SYS_STATUS_FILE")
