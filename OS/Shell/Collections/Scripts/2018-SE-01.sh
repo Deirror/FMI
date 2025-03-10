7c9abd8048f626e1bee929e070e94303985dba9f
Alex Angelow <136594831+Deirror@users.noreply.github.com>
2025-03-10 11:29:29 +0200

    Create 2018-SE-01.sh

---
+++ OS/Shell/Collections/Scripts/2018-SE-01.sh
@@ -0,0 +1,27 @@
+# // бъгаво условие, виж в pdf-a
+
+#!/bin/bash
+
+if [[ $# -ne 1 ]]; then
+    echo "Exactly one arg expected"
+    exit 1
+fi 
+
+DIR="$(echo "$1" | tr -d '/')"
+
+if [[ ! -d "$DIR" ]]; then
+    echo "Log dir with the name - $DIR - doesn't exist"
+    exit 2
+fi  
+
+FR_COUNT_MSG=""
+
+while read FRIEND; do
+    COUNT=0
+    while read FILE; do
+        COUNT=(( COUNT + $(wc -l "$FILE") ))
+    done < <(find "$DIR" -type f | grep -E "^${DIR}/[^/]+/[^/]+/${FRIEND}/[0-9]{4}-[0-9]{2}-[0-9]{2}-[0-9]{2}-[0-9]{2}-[0-9]{2}\.txt$")
+    FR_COUNT_MSG+="$FRINED $COUNT"$'\n'
+done < <(find "$DIR" -type f | grep -E "^${DIR}/[^/]+/[^/]+/[^/]+/[0-9]{4}-[0-9]{2}-[0-9]{2}-[0-9]{2}-[0-9]{2}-[0-9]{2}\.txt$" | cut -d '/' -f 4 | sort | uniq)
+
+echo "$FR_COUNT_MSG" | sort -t ' ' -k 1 -nr | head -n
