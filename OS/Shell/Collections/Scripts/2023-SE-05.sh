a632f144d10d1d126e293b3fa90a88725911540a
Alex Angelow <136594831+Deirror@users.noreply.github.com>
2025-04-09 09:20:18 +0300

    Update 2023-SE-05.sh

--- OS/Shell/Collections/Scripts/2023-SE-05.sh
+++ OS/Shell/Collections/Scripts/2023-SE-05.sh
@@ -6,5 +6,36 @@
 
 #!/bin/bash
 
+THRESHOLD=65536
 TIMES=0
+tmp_dir=$(mktemp -d)
+counts_file="$tmp_dir/counts.txt"
 
+> "$counts_file"
+
+while true; do
+    TIMES=$((TIMES + 1))
+    snapshot=$(mktemp)
+    ps -e -o rss=,comm= | awk '{arr[$2]+=$1} END {for (i in arr) print i, arr[i]}' > "$snapshot"
+
+    any_over=false
+
+    while read -r cmd mem; do
+        if [ "$mem" -gt "$THRESHOLD" ]; then
+            echo "$cmd" >> "$counts_file"
+            any_over=true
+        fi
+    done < "$snapshot"
+
+    rm "$snapshot"
+
+    if ! $any_over; then
+        break
+    fi
+
+    sleep 1
+done
+
+sort "$counts_file" | uniq -c | awk -v times="$TIMES" '$1 >= times/2 { print $2 }'
+
+rm -r "$tmp_dir"
