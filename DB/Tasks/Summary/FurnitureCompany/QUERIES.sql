02efbb3e1b34825e4bd6d05db6fcb4c2adcee23e
Alex Angelow <136594831+Deirror@users.noreply.github.com>
2025-05-23 14:14:30 +0300

    Update QUERIES.sql

--- DB/Tasks/Summary/FurnitureCompany/QUERIES.sql
+++ DB/Tasks/Summary/FurnitureCompany/QUERIES.sql
@@ -1,7 +1,7 @@
 CREATE DATABASE FurnitureCompany;
 
 CREATE TABLE Product (
-	Product_ID INTEGER PRIMARY KEY,
+	Product_ID INT PRIMARY KEY,
 	Product_Description VARCHAR(70),
 	Product_Finish VARCHAR(40),
 	Standart_Price INTEGER,
@@ -19,13 +19,18 @@ CREATE TABLE Product (
 );
 
 CREATE TABLE Customer (
-	
+	Customer_ID INT IDENTITY(1,1) PRIMARY KEY,
+	Customer_Name VARCHAR(40),
+	Customer_Address VARCHAR(50),
+	Customer_City VARCHAR(30),
+	City_Code CHAR(4)
 );
 
 CREATE TABLE Order_t (
-	
+	Order_ID INT PRIMARY KEY,
+	...
 );
 
-CREATE TABLE Order_Line (
+CREATE TABLE Order_line (
 	
 );
