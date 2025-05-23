8de5b47280db034bc168048ffaff9fd846979adc
Alex Angelow <136594831+Deirror@users.noreply.github.com>
2025-05-23 15:22:16 +0300

    Update QUERIES.sql

--- DB/Tasks/Summary/FurnitureCompany/QUERIES.sql
+++ DB/Tasks/Summary/FurnitureCompany/QUERIES.sql
@@ -101,3 +101,11 @@ GROUP BY Product.Product_ID, Product.Product_Description
 -- Напишете заявка, която извежда име на клиента и обща стойност на направените от него
 -- поръчки, само за клиентите с поръчки
 
+SELECT Customer.Customer_Name, SUM(Order_line.Ordered_Quantity*Product.Standard_Price) AS Total_Spent FROM Order_line
+JOIN Order_t
+ON Order_t.Order_ID=Order_line.Order_ID
+JOIN Customer
+ON Customer.Customer_ID=Order_t.Customer_ID
+JOIN Product
+ON Product.Product_ID=Order_line.Product_ID
+GROUP BY Customer.Customer_Name
