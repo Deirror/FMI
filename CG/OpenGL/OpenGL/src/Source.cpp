d6b127d3f3814dc01b6a4d1d603958b4af69112f
Deirror <alexmailbg@gmail.com>
2025-05-26 17:34:06 +0300

    Add OpenGL project so far

---
+++ CG/OpenGL/OpenGL/src/Source.cpp
@@ -0,0 +1,121 @@
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+
+#include <iostream>
+
+#include "Renderer.h"
+#include "VertexBuffer.h"
+#include "IndexBuffer.h"
+
+#include "VertexArray.h"
+
+#include "Shader.h"
+#include "VertexBufferLayout.h"
+#include "Texture.h"
+
+int main()
+{
+    GLFWwindow* window;
+
+    /* Initialize the library */
+    if (!glfwInit())
+        return -1;
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    /* Create a windowed mode window and its OpenGL context */
+    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    if (!window)
+    {
+        glfwTerminate();
+        return -1;
+    }
+
+    /* Make the window's context current */
+    glfwMakeContextCurrent(window);
+
+    glfwSwapInterval(1);
+
+    if (glewInit() != GLEW_OK) {
+        std::cout << "Error!\n";
+    }
+    {
+        float positions[] = {
+            // X,    Y,    U,   V
+            -0.5f, -0.5f, 0.0f, 0.0f,  // bottom-left
+             0.5f, -0.5f, 1.0f, 0.0f,  // bottom-right
+             0.5f,  0.5f, 1.0f, 1.0f,  // top-right
+            -0.5f,  0.5f, 0.0f, 1.0f   // top-left
+        };
+    unsigned int indices[] =
+    {
+        0, 1, 2,
+       0, 2, 3
+    };
+
+    glEnable(GL_BLEND);
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+
+	VertexArray va;
+    VertexBuffer vb(positions, 4 * sizeof(float) * 4);
+    
+	VertexBufferLayout layout;
+	layout.Push<float>(2); 
+	layout.Push<float>(2); 
+	va.AddVertexBuffer(vb, layout);
+
+    IndexBuffer ib(indices, 6);
+
+	Shader shader("res/shaders/Basic.shader");
+	shader.Bind();
+	shader.SetUniform4f("u_Color", 1.0f, 0.0f, 1.0f, 0.0f);
+
+	Texture texture("res/textures/images.png");
+	texture.Bind();
+	shader.SetUniform1i("u_Texture", 0);
+
+    va.Unbind();
+	vb.Unbind();
+	ib.Unbind();
+	shader.Unbind();
+
+	Renderer renderer;
+
+    float r = 0.0f;
+    float increment = 0.05f;
+
+    /* Loop until the user closes the window */
+    while (!glfwWindowShouldClose(window))
+    {
+        /* Render here */
+		renderer.Clear();
+
+        shader.Bind();
+		shader.SetUniform4f("u_Color", r, 0.0f, 1.0f, 1.0f);
+        
+		renderer.Draw(va, ib, shader);
+
+        if (r > 1.0f)
+        {
+            increment = -0.05f;
+        }
+        else if (r < 0.0f)
+        {
+            increment = 0.05f;
+        }
+
+        r += increment;
+
+        /* Swap front and back buffers */
+        glfwSwapBuffers(window);
+
+        /* Poll for and process events */
+        glfwPollEvents();
+    }
+
+}
+    glfwTerminate();
+    return 0;
+}
